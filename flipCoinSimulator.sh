c7c4c7ffbeb82bde778e1de1f64629b5d1a12e59
nomercy420 <chawhanrikesh786@gmail.com>
2021-05-17 19:34:17 +0530

    [Rikesh] added loops to flip multiple times

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,9 +1,19 @@
 #! /bin/bash
 
-flipcoin=$(( RANDOM%2 ))
-if [ $flipcoin -eq 0 ]
-then
-	echo "Heads!"
-else
-	echo "Tails"
-fi
+heads=0
+tails=0
+
+for (( counter=0; counter<30; counter++ ))
+do
+	flipcoin=$(( RANDOM%2 ))
+
+	if [ $flipcoin -eq 0 ]
+
+	then
+		heads=$(( heads+1 ))
+	else
+		tails=$(( tails+1 ))
+	fi
+done
+echo "Number of times Heads wins is : " $heads
+echo "Number of times Tails wins is : " $tails 
